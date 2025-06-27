86d76d0da5a1014afe124cbd3b2144fec97e8091
ruirui14 <2230103@ecc.ac.jp>
2025-06-27 09:44:01 +0900

    クエスト進行中UIほぼ完成

--- app/src/hooks/images.tsx
+++ app/src/hooks/images.tsx
@@ -1,10 +1,10 @@
 //画像
 const images = {
-    butonnBackGround: './src/assets/button_background.svg',     //ボタンの背景画像（ボツ）
-    arrowBack: './src/assets/arrow_back.svg',                   //戻るボタン
+    butonnBackGround: './src/assets/button_background.svg',                 //ボタンの背景画像（ボツ）
+    arrowBack: './src/assets/arrow_back.svg',                               //戻るボタン
 
-    characterLowHands: './src/assets/character_low_hands.svg',  //キャラクター（手を下げている状態）
-    
+    characterLowHands: './src/assets/character_low_hands.svg',              //キャラクター（手を下げている状態）
+    //questPlayingBackground: './src/assets/quest_playing_background.png',    //クエスト進行中の背景画像
 }
 
 export default images;
