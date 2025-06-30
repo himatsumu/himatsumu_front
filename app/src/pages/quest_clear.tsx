c2ae08046ff53a78d329b03d667904f5e87673e0
ruirui14 <2230103@ecc.ac.jp>
2025-06-30 15:53:09 +0900

    ファイル名に.moduleを追加

--- app/src/pages/quest_clear.tsx
+++ app/src/pages/quest_clear.tsx
@@ -2,7 +2,7 @@
 //ダイアログでポイント表示
 //import { useNavigate } from 'react-router-dom';
 import { useRef, useEffect, useState } from "react";
-import "../styles/quest_clear.scss";
+import styles from "../styles/quest_clear.module.scss";
 import images from "../hooks/images";
 
 export default function QuestClear() {
@@ -16,37 +16,42 @@ export default function QuestClear() {
 
         return () => clearTimeout(timer); // クリーンアップ
     }, []);
-        //参照(ref)を作る
-        // const dialogRef = useRef<HTMLDialogElement>(null);  //この参照はHTMLの<dialog>要素にくっつける(nullなので今はまだ何も参照してないよ)
-        // const handleShowModal = () => dialogRef.current?.showModal();   //showModal()　dialogを画面に表示する
-        // const handleCloseModal = () => dialogRef.current?.close();      //close()      dialogを閉じる
-        return (
-            <>
-                <div className="wrapper">
-                    <div className="backGra" />
-                    <div className="content">
-                        <img src={images.questClear} alt="クエスト達成" />
+    //参照(ref)を作る
+    // const dialogRef = useRef<HTMLDialogElement>(null);  //この参照はHTMLの<dialog>要素にくっつける(nullなので今はまだ何も参照してないよ)
+    // const handleShowModal = () => dialogRef.current?.showModal();   //showModal()　dialogを画面に表示する
+    // const handleCloseModal = () => dialogRef.current?.close();      //close()      dialogを閉じる
+    return (
+        <>
+            <div className={styles.wrapper}>
+                <div className={styles.backGra} />
+                <div className={styles.content}>
+                    <img src={images.questClear} alt="クエスト達成" className={styles.quest_success}/>
 
-                        <div className="clear-text">
+                    <div className={styles.clear_text}>
                         {isClear ? (
-                            <p>20ポイントゲット！</p>
+                            <>
+                                <div className={styles.text_side}>
+                                    <img src={images.coin} alt="コイン" />
+                                    <p>20ポイントゲット！</p>
+                                </div>
+                            </>
                         ) : (
                             <>
                                 <p>（相手の名前）が</p>
                                 <p>クエスト達成するまで待っててね</p>
                             </>
-                        )} 
-                        </div>
+                        )}
+                    </div>
 
-                        {/*キャラクター*/}
-                        <div className="image-placeholder">
-                            <img src={images.characterLowHands} alt="キャラクター手を下げている状態" />
-                        </div>
-
-
+                    {/*キャラクター*/}
+                    <div className={styles.image_placeholder}>
+                        <img src={images.characterUpHands} alt="キャラクター手を下げている状態" />
+                    </div>
 
 
-                        {/* <button type="button" onClick={handleShowModal}>ダイアログおーぷん〜</button>
+
+
+                    {/* <button type="button" onClick={handleShowModal}>ダイアログおーぷん〜</button>
 
                     ダイアログ
                     <dialog ref={dialogRef} className="quest-dialog">
@@ -57,9 +62,9 @@ export default function QuestClear() {
                         <button type="button" onClick={handleCloseModal}>今日は終了</button>
 
                     </dialog> */}
-                    </div>
                 </div>
+            </div>
 
-            </>
-        );
-    };
+        </>
+    );
+};
