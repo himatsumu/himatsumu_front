c7a76d9507db9a6c1d98b692a81774bb26338d42
ruirui14 <2230103@ecc.ac.jp>
2025-08-25 17:25:23 +0900

    遷移

--- app/src/pages/setup_finish.tsx
+++ app/src/pages/setup_finish.tsx
@@ -4,6 +4,7 @@ import styles from "../styles/setup_finish.module.scss";
 import { Button } from '../components/Button';
 //import { useEffect, useState } from "react";
 import { useNavigate } from 'react-router-dom';
+import { useLocation } from "react-router-dom";
 
 export default function SetupFinish() {
     const navigate = useNavigate();
@@ -11,17 +12,21 @@ export default function SetupFinish() {
     const handleFriendList = (() => {
         navigate("/friend-list")
     })
+
+    const location = useLocation();
+    const name = location.state?.name || "";
+
     return(
         <>
         <div className={styles.top_text}>
-            <p>あやかとももと</p>
+            <p>あやかと{name}と</p>
             <p>一緒に空き時間を</p>
             <p>これから楽しもう！</p>
         </div>
 
         <div className={styles.image_box}>
             <img src={images.characterEgg} alt="キャラクター画像" />
-            <p className={styles.chara_name}>もも</p>
+            <p className={styles.chara_name}>{name}</p>
         </div>
 
             <div className={styles.button_box} onClick={handleFriendList}>
