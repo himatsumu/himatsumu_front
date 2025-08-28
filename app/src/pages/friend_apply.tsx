3f7021163150059b0c32f9fdea882795154af4f8
ruirui14 <2230103@ecc.ac.jp>
2025-08-29 01:13:25 +0900

    申請受理GET途中

--- app/src/pages/friend_apply.tsx
+++ app/src/pages/friend_apply.tsx
@@ -7,11 +7,60 @@ import { useEffect, useState } from "react";
 import { useNavigate, useLocation } from 'react-router-dom';
 
 export default function FriendApply() {
-    const navigate = useNavigate();
 
-    const handleBack = (() => {
+    type FriendRequest = {
+        ReqID: string;
+        SenderId: string;
+        ReceverId: string;
+        SenderName: string;
+        ReqTime: string;
+    };
+
+    const navigate = useNavigate();
+    const [requests, setRequests] = useState<FriendRequest[]>([]);
+    const [error, setError] = useState("");
+
+    useEffect(() => {
+        const fetchRequest = async () => {
+            try {
+                const token = localStorage.getItem("token")
+                if (!token) {
+                    console.log("トークンがありません")
+                    alert("トークンがありません。ログインしてください")
+                    return;
+                }
+                const userId = localStorage.getItem("userId");
+
+                const response = await fetch(`http://localhost:8888/auth/request/${userId}`, {
+                    method: "GET",
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                        "Content-Type": "application/json",
+                    }
+                })
+
+                const data = await response.json();
+                console.log("リクエスト一覧", data)
+
+                if (response.ok && Array.isArray(data.Data)) {
+                    setRequests(data.Data);
+                } else {
+                    setRequests([]);
+                    setError("申請がありません");
+                }
+
+            } catch (error) {
+                console.log("リクエストの取得に失敗しました", error)
+            }
+
+        }
+        fetchRequest();
+    },[])
+
+    const handleBack = () => {
         navigate("/friend_add")
-    })
+    }
+
     return (
         <>
             <div className={styles.header}>
@@ -21,9 +70,19 @@ export default function FriendApply() {
                 <h1 className={styles.top_text}>申請リクエスト</h1>
             </div>
 
-            {/* <div className={styles.component_card}>
-                <FriendCard />
-            </div> */}
+            <div className={styles.request_list}>
+                {requests.length > 0 ? (
+                    requests.map((req) => (
+                        <FriendCard
+                            key={req.ReqID}
+                            name={req.SenderName}
+                            id={req.SenderId}
+                        />
+                    ))
+                ) : (
+                    <p className={styles.error_text}>{error}</p>
+                )}
+            </div>
 
 
         </>
--- app/src/pages/login.tsx
+++ app/src/pages/login.tsx
@@ -41,28 +41,28 @@ export default function Login() {
     }
 
     //Googleログイン後にtokenを取得
-    useEffect(() => {
-        const fetchToken = async () => {
-            try {
-                const res = await fetch("http://localhost:18080/auth/user/", {
-                    credentials: "include", // cookie送るために必要
-                });
-                const data = await res.json();
+    // useEffect(() => {
+    //     const fetchToken = async () => {
+    //         try {
+    //             const res = await fetch("http://localhost:18080/auth/user/", {
+    //                 credentials: "include", // cookie送るために必要
+    //             });
+    //             const data = await res.json();
 
-                if (data.token) {
-                    localStorage.setItem("token", data.token);
-                    setIsLoggedIn(true);
-                    console.log("トークン取得成功！");
-                    navigate("/user_setup");
-                } else {
-                    console.error("トークンが含まれてないよ");
-                }
-            } catch (err) {
-                console.error("トークン取得失敗", err);
-            }
-        };
-        fetchToken();
-    }, [location]);
+    //             if (data.token) {
+    //                 localStorage.setItem("token", data.token);
+    //                 setIsLoggedIn(true);
+    //                 console.log("トークン取得成功！");
+    //                 navigate("/user_setup");
+    //             } else {
+    //                 console.error("トークンが含まれてないよ");
+    //             }
+    //         } catch (err) {
+    //             console.error("トークン取得失敗", err);
+    //         }
+    //     };
+    //     fetchToken();
+    // }, [location]);
 
     //メールとパスワードでログイン
     const handleLogin = async () => {
@@ -96,23 +96,23 @@ export default function Login() {
     }
 
     //Googleログイン
-    const handleGoogleLogin = () => {
-        window.location.href = "http://localhost:18080/auth/google";
+    // const handleGoogleLogin = () => {
+    //     window.location.href = "http://localhost:18080/auth/google";
 
-        //     //     try {
-        //     //         const res = await fetch("http://localhost:18080/auth/google");
-        //     //         const data = await res.json();
+    //     //     //     try {
+    //     //     //         const res = await fetch("http://localhost:18080/auth/google");
+    //     //     //         const data = await res.json();
 
-        //     //         if (data.url) {
-        //     //             window.location.href = data.url;
-        //     //         } else {
-        //     //             console.error("認証URLが見つかりません");
-        //     //         }
-        //     //     } catch (error) {
-        //     //         console.error("認証取得エラー:", error);
-        //     //     }
+    //     //     //         if (data.url) {
+    //     //     //             window.location.href = data.url;
+    //     //     //         } else {
+    //     //     //             console.error("認証URLが見つかりません");
+    //     //     //         }
+    //     //     //     } catch (error) {
+    //     //     //         console.error("認証取得エラー:", error);
+    //     //     //     }
 
-    };
+    // };
 
 
     return (
@@ -148,9 +148,9 @@ export default function Login() {
             <div className={styles.button_container}>
                 <Button variant="buttonLogin" className={styles.login_button} onClick={handleLogin}>ログイン</Button>
                 <p className={styles.form_divider}>または</p>
-                <button onClick={handleGoogleLogin} className={styles.google_button}>
+                {/* <button onClick={handleGoogleLogin} className={styles.google_button}>
                     <img src={images.GoogleButton} alt="Google認証" />
-                </button>
+                </button> */}
             </div>
 
 
