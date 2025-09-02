8cffcc474c2317cc8c192cb2ace65ee8a8add449
toma1128 <kirima--idea--@au.com>
2025-09-02 14:15:54 +0900

    [update]デプロイ先でも画像が見れるように修正

--- app/src/hooks/images.tsx
+++ app/src/hooks/images.tsx
@@ -1,68 +1,95 @@
+import butonnBackGround from '../assets/button_background.svg';
+import arrowBack from '../assets/arrow_back.svg';
+import arrowBackBlack from '../assets/arrow_back_black.svg';
+import characterLowHands from '../assets/character_low_hands.svg';
+import characterCry from '../assets/character_cry.svg';
+import characterUpHands from '../assets/character_up_hands.svg';
+import characterEgg from '../assets/character_egg.svg';
+import egg1 from '../assets/egg1.svg';
+import egg2 from '../assets/egg2.svg';
+import egg3 from '../assets/egg3.svg';
+import egg4 from '../assets/egg4.svg';
+import questNo from '../assets/quest_failed.svg';
+import questClear from '../assets/quest_successful.svg';
+import coin from '../assets/coin.svg';
+import radialCircle from '../assets/radial_circle.svg';
+import album from '../assets/album.svg';
+import dressUp from '../assets/dressUp.svg';
+import friendAdd from '../assets/friendAdd.svg';
+import faceOnly from '../assets/faceOnly.svg';
+import GoogleButton from '../assets/web_light_rd_SI.svg';
+import GoogleButtonSU from '../assets/web_light_rd_SU.svg';
+import closeButton from '../assets/close.svg';
+import search from '../assets/search.svg';
+import arrowForward from '../assets/arrow_forward_ios.svg';
+import human from '../assets/human.svg';
+import shuffle from "../assets/shuffle.svg";
+import albumFolder from '../assets/folder.svg';
+import DressupCharacter from "../assets/dressup_character.svg";
+import Suit from "../assets/suit.svg";
+import SuitThumb from "../assets/suit_thumb.svg";
+import ChildThumb from "../assets/child_thumb.svg";
+import MaidThumb from "../assets/maid_thumb.svg";
+import RaincoatThumb from "../assets/raincoat_thumb.svg";
+import SantaThumb from "../assets/santa_thumb.svg";
+import MarineThumb from "../assets/marine_thumb.svg";
+import InsectThumb from "../assets/insect_thumb.svg";
+import SuitHatThumb from "../assets/suit_hat_thumb.svg";
+import ChildHatThumb from "../assets/child_hat_thumb.svg";
+import MaidHatThumb from "../assets/maid_hat_thumb.svg";
+import SantaHatThumb from "../assets/santa_hat_thumb.svg";
+import MarineHatThumb from "../assets/marine_hat_thumb.svg";
+import InsectHatThumb from "../assets/insect_hat_thumb.svg";
+import ChildBelongingsThumb from "../assets/child_belongings_thumb.svg";
+import InsectBelongingsThumb from "../assets/insect_belongings_thumb.svg";
+import InsectBelongingsBagThumb from "../assets/insect_belongings2_thumb.svg";
 
-//画像
 const images = {
-    butonnBackGround: './src/assets/button_background.svg',                 //ボタンの背景画像（ボツ）
-    arrowBack: './src/assets/arrow_back.svg',                               //戻るボタン
-    arrowBackBlack: './src/assets/arrow_back_black.svg',                     //戻るボタン（黒）
-
-    //キャラクター
-    characterLowHands: './src/assets/character_low_hands.svg',              //キャラクター（手を下げている状態）
-    characterCry: './src/assets/character_cry.svg',                         //キャラクター（泣いている状態）
-    characterUpHands: './src/assets/character_up_hands.svg',                //キャラクター（手を挙げている状態）
-    characterEgg: './src/assets/character_egg.svg',                         //キャラクター（たまご）
-    egg1: './src/assets/egg1.svg',                                          //卵割れ１
-    egg2: './src/assets/egg2.svg',                                          //卵割れ2
-    egg3: './src/assets/egg3.svg',                                          //卵割れ3
-    egg4: './src/assets/egg4.svg',                                          //卵割れ4
-
-    questNo: './src/assets/quest_failed.svg',                               //「クエスト失敗」
-    questClear: './src/assets/quest_successful.svg',                        //「クエスト達成」
-
-
-    coin: './src/assets/coin.svg',                                          //コイン
-    radialCircle: './src/assets/radial_circle.svg',                         //放射状の背景
-    album: './src/assets/album.svg',                                        //アルバム
-    dressUp: './src/assets/dressUp.svg',
-    friendAdd: './src/assets/friendAdd.svg',
-    faceOnly: './src/assets/faceOnly.svg',
-    GoogleButton: './src/assets/web_light_rd_SI.svg',                       //google認証のためのボタン
-    GoogleButtonSU: './src/assets/web_light_rd_SU.svg',                     //google認証のためのボタン(サインアップ)
-    closeButton: './src/assets/close.svg',                                  //閉じるボタン
-    search: './src/assets/search.svg',                                      //虫眼鏡
-    arrowForward: './src/assets/arrow_forward_ios.svg',                     //申請リクエスト　>
-    human: './src/assets/human.svg',
-    albumFolder: './src/assets/folder.svg'
-    human: './src/assets/human.svg',                                        //申請の人間
-    shuffle: "./src/assets/shuffle.svg",                                    //名前ランダム生成
-
-    DressupCharacter: "./src/assets/dressup_character.svg",                 //着せ替えキャラ
-
-    //服
-    Suit: "./src/assets/suit.svg",                                          //スーツ（重ねるよう）
-    SuitThumb: "./src/assets/suit_thumb.svg",                               //スーツ（アイテム欄）
-    ChildThumb: "./src/assets/child_thumb.svg",                             //幼稚園児の幅（アイテム欄）
-    MaidThumb: "./src/assets/maid_thumb.svg",                               //メイド（アイテム欄）
-    RaincoatThumb: "./src/assets/raincoat_thumb.svg",                       //レインコート（アイテム欄）
-    SantaThumb: "./src/assets/santa_thumb.svg",                             //サンタ（アイテム欄）
-    MarineThumb: "./src/assets/marine_thumb.svg",                           //マリン（アイテム欄）
-    InsectThumb: "./src/assets/insect_thumb.svg",                           //虫取り（アイテム欄）
-
-    //帽子
-    SuitHatThumb: "./src/assets/suit_hat_thumb.svg",                        //スーツ（アイテム欄）
-    ChildHatThumb: "./src/assets/child_hat_thumb.svg",                      //幼稚園児（アイテム欄）
-    MaidHatThumb: "./src/assets/maid_hat_thumb.svg",                        //メイド（アイテム欄）
-    SantaHatThumb: "./src/assets/santa_hat_thumb.svg",                      //サンタ（アイテム欄）
-    MarineHatThumb: "./src/assets/marine_hat_thumb.svg",                    //マリン（アイテム欄）
-    InsectHatThumb: "./src/assets/insect_hat_thumb.svg",                    //虫取り（アイテム欄）
-
-    //持ち物
-    ChildBelongingsThumb: "./src/assets/child_belongings_thumb.svg",        //幼稚園児（アイテム欄）
-    InsectBelongingsThumb: "./src/assets/insect_belongings_thumb.svg",      //虫取り（アイテム欄）
-    InsectBelongingsBagThumb: "./src/assets/insect_belongings2_thumb.svg",  //虫取り（アイテム欄）
-}
+    butonnBackGround,
+    arrowBack,
+    arrowBackBlack,
+    characterLowHands,
+    characterCry,
+    characterUpHands,
+    characterEgg,
+    egg1,
+    egg2,
+    egg3,
+    egg4,
+    questNo,
+    questClear,
+    coin,
+    radialCircle,
+    album,
+    dressUp,
+    friendAdd,
+    faceOnly,
+    GoogleButton,
+    GoogleButtonSU,
+    closeButton,
+    search,
+    arrowForward,
+    human,
+    shuffle,
+    albumFolder,
+    DressupCharacter,
+    Suit,
+    SuitThumb,
+    ChildThumb,
+    MaidThumb,
+    RaincoatThumb,
+    SantaThumb,
+    MarineThumb,
+    InsectThumb,
+    SuitHatThumb,
+    ChildHatThumb,
+    MaidHatThumb,
+    SantaHatThumb,
+    MarineHatThumb,
+    InsectHatThumb,
+    ChildBelongingsThumb,
+    InsectBelongingsThumb,
+    InsectBelongingsBagThumb,
+};
 
 export default images;
-
-
-
-
